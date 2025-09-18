86295b3d62b26000d1e3cdc1e040e10ae617bf62
Jelena Kovač <jelena.kovac765@gmail.com>
2025-09-18 17:00:48 +0200

    init: initial projects

---
+++ pravna/src/main/java/com/example/pravna/PravnaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pravna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PravnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PravnaApplication.class, args);
+	}
+
+}
---
+++ pravna/src/test/java/com/example/pravna/PravnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pravna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PravnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
