81b77295d0bf06aac12f113278013e969b1ab834
Jelena Kovač <jelena.kovac765@gmail.com>
2025-10-02 16:04:01 +0200

    feat: adding new controller class and model class

--- pravna/src/main/java/com/example/pravna/controller/VerdictsController.java
+++ pravna/src/main/java/com/example/pravna/controller/VerdictsController.java
@@ -1,4 +1,13 @@
 package com.example.pravna.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/legal-cases")
+@RequiredArgsConstructor
+@Slf4j
 public class VerdictsController {
 }
