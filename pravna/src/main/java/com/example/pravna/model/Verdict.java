f20184fe16ea339d91550c6e491a6713e0f6ba66
Jelena Kovač <jelena.kovac765@gmail.com>
2025-10-04 14:03:06 +0200

    feat: updating verdict class

--- pravna/src/main/java/com/example/pravna/model/Verdict.java
+++ pravna/src/main/java/com/example/pravna/model/Verdict.java
@@ -21,5 +21,47 @@ public class Verdict {
 
     @Column(name = "name", nullable = false)
     @NotBlank(message = "Case name is required")
-    private String name;
+    private String name; // broj predmeta
+
+    @Column(name = "optuzeniSluzbenoLice", nullable = false)
+    @NotBlank(message = "Case optuzeniSluzbenoLice is required")
+    private boolean optuzeniSluzbenoLice;
+
+    @Column(name = "optuzeniDavalacMita", nullable = false)
+    @NotBlank(message = "Case optuzeniDavalacMita is required")
+    private boolean optuzeniDavalacMita;
+
+    @Column(name = "optuzeniPrimalacMita", nullable = false)
+    @NotBlank(message = "Case optuzeniPrimalacMita is required")
+    private boolean optuzeniPrimalacMita;
+
+    @Column(name = "optuzeniZahtevaoMito", nullable = false)
+    @NotBlank(message = "Case optuzeniZahtevaoMito is required")
+    private boolean optuzeniZahtevaoMito;
+
+    @Column(name = "optuzebiPrihvatioObecanjeMita", nullable = false)
+    @NotBlank(message = "Case optuzebiPrihvatioObecanjeMita is required")
+    private boolean optuzebiPrihvatioObecanjeMita;
+
+    @Column(name = "radnjaNezakonita", nullable = false)
+    @NotBlank(message = "Case radnjaNezakonita is required")
+    private boolean radnjaNezakonita; // da li mito dato da se obavi nesto sto se ne smije
+
+    @Column(name = "radnjaNeizvrsavanja", nullable = false)
+    @NotBlank(message = "Case radnjaNeizvrsavanja is required")
+    private boolean radnjaNeizvrsavanja;
+
+    @Column(name = "oslobadjajuceOkolnosti", nullable = false)
+    @NotBlank(message = "Case oslobadjajuceOkolnosti is required")
+    private int oslobadjajuceOkolnosti;
+
+
+    @Column(name = "utvrdjenaKrivicaUPresudi", nullable = false)
+    @NotBlank(message = "Case name is required")
+    private boolean utvrdjenaKrivicaUPresudi;
+
+    @Column(name = "duzinaKazne", nullable = false)
+    private int duzinaKazne; // mjeseci
+
+
 }
